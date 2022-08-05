adce5eb886ba95c56fa68eb82a801aa63b827d62
Alberto Pelosi <a.pelosi@codermine.com>
2022-08-05 11:32:24 +0200

    create database with UTF8 encoding

--- prepare_database.go
+++ prepare_database.go
@@ -79,7 +79,7 @@ func defaultCreateDatabase(port uint32, username, password, database string) (er
 		err = connectionClose(db, err)
 	}()
 
-	if _, err := db.Exec(fmt.Sprintf("CREATE DATABASE %s", database)); err != nil {
+	if _, err := db.Exec(fmt.Sprintf("CREATE DATABASE %s WITH TEMPLATE template0 ENCODING 'UTF8'", database)); err != nil {
 		return errorCustomDatabase(database, err)
 	}
 
